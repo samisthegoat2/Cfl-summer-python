8f40176047b289d39c426e637d45493067a3fa3f
samisthegoat2 <samuelchavezp126@gmail.com>
2026-07-13 16:24:19 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+
+name= input("Enter a name: ")
+food= input("choose your faviorite food: ")
+sign= input("Please enter a zodiac sign: ") 
+sport= input("please enter a sport:")
+
+print(f"My name is {name} and i lke to eat {food} and i am a {sign}.")
+print(f"I also like to play {sport} becuse its very fun")
